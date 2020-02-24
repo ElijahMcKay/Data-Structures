5a8ae98d8bfe9662c959c40e87ca40fc16c1c14d
Elijah McKay <elijahmckay10@gmail.com>
2020-02-23 21:40:47 -0600

    finished stacks and queues part of MVP

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,34 @@
-import sys
-sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
-
 
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
+
 
     def enqueue(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        # checking to see if the LL is empty
+        if not self.isEmpty():
+            self.size -= 1
+            # self.storage.length -= 1
+            return self.storage.remove_from_head()
+        # if it is empty, just return
+        else:
+            return
 
     def len(self):
-        pass
+        return self.size
+
+    # read an article mentioning the common methods on a stack and isEmpty was mentioned
+    def isEmpty(self):
+        if self.size == 0:
+            return 1
+        else:
+            return 0
+
+
