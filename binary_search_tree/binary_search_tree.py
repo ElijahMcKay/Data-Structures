81989d7383183f939745d6871fc7e8625fe9be3c
Elijah McKay <elijahmckay10@gmail.com>
2020-02-26 00:51:50 -0600

    finished insert method on BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,10 +1,13 @@
-import sys
-sys.path.append('../queue_and_stack')
 from dll_queue import Queue
 from dll_stack import Stack
 
 
 class BinarySearchTree:
+    """
+    Notes:
+    Each node also has to be a binary search tree
+
+    """
     def __init__(self, value):
         self.value = value
         self.left = None
@@ -12,15 +15,59 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        """ 
+        compare root node
+        if lesser go to left child
+        if greater or = to to right child
+        if no child on that side, insert
+        else try again starting from the child on appropriate side (recursion)
+        """
+        # creating a variable for whatever new node will be so we can return it
+        new_node_tree = None
+        # if curr tree val less than or equal to inserted val
+        if self.value <= value:
+            # and the .right node already exists, we have to keep going
+            if self.right:
+                # so we recursively call self.right.insert with the given value
+                return self.right.insert(value)
+            # if right node doesn't exist, create it
+            else:
+                self.right = BinarySearchTree(value)
+                # assigning this var so we can return what we just created
+                new_node_tree = self.right
+
+        # if curr tree val greater than or equal to inserted val
+        elif self.value >= value:
+            # and the .left node already exists, we have to keep going
+            if self.left:
+                # so we recursively call self.left.insert with the given value
+                return self.left.insert(value)
+            # if left node doesn't exist, create it
+            else:
+                self.left = BinarySearchTree(value)
+                # assigning this var so we can return what we just created
+                new_node_tree = self.left
+
+        return new_node_tree
+
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        """
+        look at root, if root is it, return
+        if value is less than node, go left and repeat.  If no left child, return none
+        if value is >= node, go right and repeat.  If no left child, return none
+        """
+        # if self.value == target:
+        #     return self
+        # else
 
     # Return the maximum value found in the tree
     def get_max(self):
+        """
+        keep going right until no more right
+        """
         pass
 
     # Call the function `cb` on the value of each node
